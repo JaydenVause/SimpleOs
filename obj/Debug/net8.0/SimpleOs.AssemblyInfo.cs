fef96cac615b3d5830d2021ae3323753b9de76e5
Jayden Vause <jaydenvausemailbox@gmail.com>
2024-06-29 20:22:32 +1000

    Created basic CLI for kernel

--- obj/Debug/net8.0/SimpleOs.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpleOs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a422b97cf1563507d62c31bb27330ccf5c2b3dd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c912e2360ed836724a7c376ee5c14f7c56c6bfcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
